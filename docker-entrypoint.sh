6b85d7db10b70f9257403dbc6069d7cc2f3a944f
root <root@yggdrasil>
2019-02-03 22:49:50 +0000

    Initial commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ `ls /etc/apache2/sites-available/ | wc -l` -eq 0 ]
+then
+  cp -r /etc/apache2/sites-available_default/* /etc/apache2/sites-available/
+fi
+
+if [ `ls /etc/letsencrypt/ | wc -l` -eq 0 ]
+then
+  cp -r /etc/letsencrypt_default/* /etc/letsencrypt/
+fi
+
+find /var/www/html/includes ! -name 'autoconf.php' -type f -exec rm -f {} +
+cp -r /var/www/html/includes_default/* /var/www/html/includes/
+chown www-data:www-data /var/www/html/includes
+
+if [ -f /var/www/html/includes/autoconf.php ]
+then
+  rm -r /var/www/html/installer
+fi
+
+#List site and enable
+ls /etc/apache2/sites-available/ -1A | a2ensite *.conf
+
+#LETSECNRYPT
+if [ "$LETSENCRYPTDOMAINS" != "" ]
+then
+  /usr/sbin/apache2ctl start
+  certbot --apache -n -d $LETSENCRYPTDOMAINS --agree-tos --email $LETSENCRYPTEMAIL
+  /usr/sbin/apache2ctl stop
+fi
+
+#Start Cron
+/etc/init.d/anacron start
+/etc/init.d/cron start
+
+#Launch Apache on Foreground
+/usr/sbin/apache2ctl -D FOREGROUND
